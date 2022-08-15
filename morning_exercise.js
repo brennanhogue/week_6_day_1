6c2af181ee71e9e42852c7af2848585f6d0ef427
Brennan Hogue <99865645+brennanhogue@users.noreply.github.com>
2022-08-15 15:37:56 -0600

    Add files via upload

---
+++ morning_exercise.js
@@ -0,0 +1,9 @@
+function createTower(n) {
+    let towerArr = [];
+    for (let i = 0; i < n; i++) {
+      towerArr.push(
+        " ".repeat(n - i - 1) + "*".repeat(i * 2 + 1) + " ".repeat(n - i - 1)
+      );
+    }
+    return towerArr;
+  }
